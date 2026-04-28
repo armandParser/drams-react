148b67fb0d3e5b6e773aaf72d0cc11b59d2b6aa8
Armand Collier <schneideretcollier.innovation@gmail.com>
2026-04-28 15:58:22 +0200

    fix netlify

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as VerticalSlider } from './components/VerticalSlider';
-export { default as StandardSwitch } from './components/StandardSwitch';
-export { default as PinchinSwitch } from './components/PinchinSwitch';
-export { default as RollinSwitch } from './components/RollinSwitch';
-export { default as SquareDotsSwitch } from './components/SquareDotsSwitch';
-export { default as IOSwitch } from './components/IOSwitch';
-export { default as LongSwitch } from './components/LongSwitch';
-export { default as ThreeStateSwitch } from './components/ThreeStateSwitch';
